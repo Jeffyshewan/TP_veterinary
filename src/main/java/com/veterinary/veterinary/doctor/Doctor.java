a0067113a52eacb02af1ae7130e50800f39ea746
Chiara <brichot.chiara@yahoo.fr>
2020-11-24 14:41:10 +0100

    :sparkles: : add controller repo & class : doctor

---
+++ src/main/java/com/veterinary/veterinary/doctor/Doctor.java
@@ -0,0 +1,29 @@
+package com.veterinary.veterinary.doctor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Doctor {
+    @Id
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String address;
+    private int zipcode;
+    private Date arrival;
+
+    @OneToMany
+    @JoinColumn(name = "doctor_id")
+    private Set<Doctor> doctors;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- Doctors
+insert into doctor(id, firstname, lastname, address, zipcode, arrival) values (1, 'Yolande', 'Moreau', '3 rue Deschiens', '59000','03/02/2002');
+insert into doctor(id, firstname, lastname, address, zipcode, arrival) values (1, 'Heloise', 'Letissier', '5 rue Bowie', '95360','16/10/2010');
+insert into doctor(id, firstname, lastname, address, zipcode, arrival) values (1, 'Jenny', 'Letellier', '105 rue a l envers', '93000','11/12/2008');
+insert into doctor(id, firstname, lastname, address, zipcode, arrival) values (1, 'Chiara', 'Brichot', '10 rue de maubeuge', '59600','20/04/2019');
+insert into doctor(id, firstname, lastname, address, zipcode, arrival) values (1, 'Pablo', 'Picasso', '3 avenue d Antibes', '02000','23/01/2012');
+insert into doctor(id, firstname, lastname, address, zipcode, arrival) values (1, 'Frank', 'Fontaine', '363 bis bvd rapture', '75000','03/11/2007');
